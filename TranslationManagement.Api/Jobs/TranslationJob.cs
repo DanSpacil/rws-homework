d1571716c5339c5e04a443203a37ad286b147281
Dan Spacil <danielspacil@outlook.com>
2024-07-19 16:39:30 +0200

    WIP: changing laptops

--- TranslationManagement.Api/Jobs/TranslationJob.cs
+++ TranslationManagement.Api/Jobs/TranslationJob.cs
@@ -4,8 +4,21 @@ public class TranslationJob
 {
     public int Id { get; set; }
     public string CustomerName { get; set; }
-    public string Status { get; set; }
+
+    public JobStatus Status { get; set; }
     public string OriginalContent { get; set; }
     public string TranslatedContent { get; set; }
-    public double Price { get; set; }
+    public double Price { get; private set; }
+
+    public void SetPrice(double pricePerCharacter)
+    {
+        this.Price = this.OriginalContent.Length * pricePerCharacter;
+    }
+}
+
+public enum JobStatus
+{
+    New,
+    InProgress,
+    Completed
 }
--- TranslationManagement.Api/Jobs/TranslationJobService.cs
+++ TranslationManagement.Api/Jobs/TranslationJobService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TranslationManagement.Api.Persistence;
@@ -18,4 +19,31 @@ public class TranslationJobService : ITranslationJobService
     {
         return await _appContext.TranslationJobs.ToListAsync();
     }
+
+    public async Task<JobCreatedResult> CreateJob(CreateJobCommand createJobCommand)
+    {
+        var job = new TranslationJob { CustomerName = createJobCommand.CustomerName, Status = JobStatus.New, OriginalContent = createJobCommand.OriginalContent };
+        var entity = await _appContext.TranslationJobs.AddAsync(job);
+        await _appContext.SaveChangesAsync();
+        return entity.Entity.Id;
+    }
+}
+
+public record JobCreatedResult
+{
+    [NotNullWhen() public int? JobId { get; private init; }
+
+    public bool IsSuccess { get; private init; }
+
+    public static JobCreatedResult Success(int jobId)
+        => new () { IsSuccess = true, JobId = jobId };
+
+    public static JobCreatedResult Error()
+        => new () { IsSuccess = true };
+}
+
+public record CreateJobCommand
+{
+    public string CustomerName { get; set; }
+    public string OriginalContent { get; set; }
 }
