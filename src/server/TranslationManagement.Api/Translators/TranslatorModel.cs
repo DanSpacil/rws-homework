d481fc726f1a604c6a98a544d7fcc7af64ba50f9
Dan Spáčil <danielspacil@outlook.com>
2024-07-20 22:52:22 +0200

    Adjusted folder structure for client

---
+++ src/server/TranslationManagement.Api/Translators/TranslatorModel.cs
@@ -0,0 +1,10 @@
+namespace TranslationManagement.Api.Translators;
+
+public class TranslatorModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string HourlyRate { get; set; }
+    public TranslatorStatus Status { get; set; }
+    public string CreditCardNumber { get; set; }
+}
